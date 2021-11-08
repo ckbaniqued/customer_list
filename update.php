764dee0de736b8fbca168aad44cd7f7e529b4ddb
ckbaniqued <93855777+ckbaniqued@users.noreply.github.com>
2021-11-08 16:06:57 +0800

    Update update.php

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect("3.109.187.205", "root", "royale1987", "db_automation");
+$connect = mysqli_connect("3.108.184.34", "royale", "royale1987", "db_automation");
 if(isset($_POST["id"]))
 {
  $value = mysqli_real_escape_string($connect, $_POST["value"]);
